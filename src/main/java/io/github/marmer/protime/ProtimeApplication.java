01ee769f9ae25f6e9ada3255e5f17f1887f2acda
MarMer <1055445+marmer@users.noreply.github.com>
2017-09-27 23:35:50 +0200

    spring boot base

---
+++ src/main/java/io/github/marmer/protime/ProtimeApplication.java
@@ -0,0 +1,12 @@
+package io.github.marmer.protime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtimeApplication.class, args);
+	}
+}
