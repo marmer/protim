ff39fbfd3974f793779eeadb42637132c638472e
MarMer <1055445+marmer@users.noreply.github.com>
2017-10-02 09:50:10 +0200

    added another version for flyway to watch the changes

--- src/main/resources/db/migration/V2__init2.sql
+++ src/main/resources/db/migration/V2__init2.sql
@@ -1,11 +1,11 @@
-CREATE TABLE sample_model (
+CREATE TABLE sample_model2 (
 	id int8 NOT NULL,
 	nice_property varchar(100) NULL,
 	"version" int8 NOT NULL,
-	CONSTRAINT sample_model_pk PRIMARY KEY (id)
+	CONSTRAINT sample_model2_pk PRIMARY KEY (id)
 );
 
-CREATE SEQUENCE sample_model_seq
+CREATE SEQUENCE sample_model_seq2
 INCREMENT BY 50
 MINVALUE 1
 MAXVALUE 9223372036854775807
