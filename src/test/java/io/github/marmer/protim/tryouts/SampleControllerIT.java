7a4e2c5c4be68b557df6e5609de300955098ea55
MarMer <1055445+marmer@users.noreply.github.com>
2017-10-02 19:06:47 +0200

    example configuration for semi-automatic rest api documentation

--- src/test/java/io/github/marmer/protim/tryouts/SampleControllerIT.java
+++ src/test/java/io/github/marmer/protim/tryouts/SampleControllerIT.java
@@ -4,6 +4,11 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -14,7 +19,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
 import org.springframework.test.context.junit4.rules.SpringClassRule;
 import org.springframework.test.context.junit4.rules.SpringMethodRule;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -25,8 +31,7 @@ import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
 @WebAppConfiguration
-@ContextConfiguration
-public class SampleControllerIT {
+public class SampleControllerIT{
 
 	@Autowired
 	private WebApplicationContext context;
@@ -41,9 +46,16 @@ public class SampleControllerIT {
 	@Rule
 	public final SpringMethodRule springMethodRule = new SpringMethodRule();
 
+	@Rule
+	public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation("target/generated-snippets");
+
+	private RestDocumentationResultHandler document;
+
 	@Before
 	public void setup() {
-		mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+		this.document = document("{method-name}", preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint()));
+		mockMvc = MockMvcBuilders.webAppContextSetup(context).apply(documentationConfiguration(this.restDocumentation))
+				.build();
 		sampleModelRepository.deleteAll();
 		sampleModelRepository.flush();
 	}
@@ -61,6 +73,7 @@ public class SampleControllerIT {
 		assertThat(sampleModelRepository.findAll(), hasSize(1));
 		mockMvc.perform(get("/sample"));
 		assertThat(sampleModelRepository.findAll(), hasSize(2));
+		document.handle(request.andReturn());
 	}
 
 }
