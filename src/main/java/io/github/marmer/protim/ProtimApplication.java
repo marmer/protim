90c0908d0ed0037e700e25d4aed37821fe4f79b0
MarMer <1055445+marmer@users.noreply.github.com>
2017-10-17 23:23:13 +0200

    unneccessary user creation removed

--- src/main/java/io/github/marmer/protim/ProtimApplication.java
+++ src/main/java/io/github/marmer/protim/ProtimApplication.java
@@ -1,12 +1,7 @@
 package io.github.marmer.protim;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import io.github.marmer.protim.model.Useraccount;
-import io.github.marmer.protim.repositories.UseraccountRepository;
 
 @SpringBootApplication
 public class ProtimApplication {
@@ -25,21 +20,4 @@ public class ProtimApplication {
 	// TODO store passwords hashed with salts
 	// TODO User-Management (creation and management)
 
-	@Bean
-	CommandLineRunner init(final UseraccountRepository accountRepository) {
-
-		return new CommandLineRunner() {
-
-			@Override
-			public void run(final String... arg0) throws Exception {
-				final String username = "protim";
-				// TODO this should not happen
-				if (accountRepository.findByUsername(username) == null) {
-					accountRepository.save(Useraccount.builder().username(username).password("protimpw").build());
-				}
-			}
-
-		};
-
-	}
 }
