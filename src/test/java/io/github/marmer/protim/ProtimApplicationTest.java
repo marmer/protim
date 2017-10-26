185b49291137c45c117289375a00f7e5a48f3a76
MarMer <1055445+marmer@users.noreply.github.com>
2017-10-27 00:15:26 +0200

    test class renamed

--- src/test/java/io/github/marmer/protim/ProtimApplicationTest.java
+++ src/test/java/io/github/marmer/protim/ProtimApplicationTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ProtimApplicationTests {
+public class ProtimApplicationTest {
 
 	@Test
 	public void contextLoads() {
