757e25e60aeb290851cb54ee0a88b469d704783e
MarMer <1055445+marmer@users.noreply.github.com>
2018-03-29 00:13:32 +0200

    - npm run eject - lots of configuration to make webpack work

---
+++ src/main/js/webpack.config.js
@@ -0,0 +1,70 @@
+// var path = require('path');
+//
+// module.exports = {
+//     entry: './src/App.tsx',
+//     devtool: 'sourcemaps',
+//     cache: true,
+//     output: {
+//         path: path.resolve(__dirname, 'src', 'main', 'resources', 'static'),
+//         filename: 'bundle.js'
+//     },
+//     resolve: {
+//         extensions: [
+//             '',
+//             '.tsx',
+//             '.ts',
+//             '.js',
+//             '.tsx'
+//         ]
+//     },
+//     module: {
+//         rules: [
+//             {
+//                 test: path.join(__dirname, '.'),
+//                 exclude: /(node_modules)/,
+//                 use: 'ts-loader'
+//             }
+//         ]
+//     }
+// };
+
+const path = require('path');
+
+module.exports = {
+    entry: './src/App.tsx',
+    devtool: 'inline-source-map',
+    cache: true,
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.svg$/,
+                loaders: [
+                    'babel-loader',
+                    {
+                        loader: 'react-svg-loader',
+                        query: {
+                            jsx: true
+                        }
+                    },
+                ]
+            },
+            {
+                test: /\.css?$/,
+                use: 'css-loader',
+                exclude: /node_modules/
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'src', 'main', 'resources', 'static')
+    }
+};
