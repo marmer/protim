463dc8d83ac333974a73efaaaa3f58029c6b8716
MarMer <1055445+marmer@users.noreply.github.com>
2018-04-07 19:57:28 +0200

    Some (broken) try to use a mocked Iso8601Service

--- src/main/js/src/ui/TimeManagementView.test.tsx
+++ src/main/js/src/ui/TimeManagementView.test.tsx
@@ -4,6 +4,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {shallow, ShallowWrapper} from 'enzyme';
 import {TimeManagementView} from './TimeManagementView';
+import {Iso8601Service} from "../services/Iso8601Service";
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
@@ -23,6 +24,13 @@ function todayAsIsoString(): String {
 
 describe(`<${TimeManagementView.name} />`, () => {
     let tree: ShallowWrapper;
+
+    let iso8601ServiceMock = jest.mock(Iso8601Service.name);
+    iso8601ServiceMock.mock(Iso8601Service.today.name, () => {
+        return "1985-01-02"
+    });
+    // TODO: MarMer 07.04.2018 Get out how to mock the function "today" correctly!
+
     beforeAll(() => {
         tree = shallow(<TimeManagementView/>);
     });
