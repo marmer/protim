41d1072786b9cf97bd722f15c6ece2f954a1082a
MarMer <1055445+marmer@users.noreply.github.com>
2018-04-12 17:43:31 +0200

    direct mockito usage removed again for some progress

--- src/main/js/src/ui/TimeManagementView.test.tsx
+++ src/main/js/src/ui/TimeManagementView.test.tsx
@@ -7,7 +7,6 @@ import {TimeManagementView} from './TimeManagementView';
 import {SystemTimeService} from '../service/SystemTimeService';
 import {BookingDayView} from "./BookingDayView";
 import {DateTimeService} from "../service/DateTimeService";
-import * as Mockito from 'ts-mockito'
 import fn = jest.fn;
 
 it('renders without crashing', () => {
@@ -70,14 +69,8 @@ describe(`<${TimeManagementView.name} />`, () => {
 
         it("should go to the next day on click", () => {
             // preparation
-
-            Mockito.when(DateTimeService.dayAfter)
-            ######
             DateTimeService.dayAfter = fn((day) => {
-                if (day === today) {
-                    return nextDay;
-                }
-                return null;
+                return day === today ? nextDay : null;
             });
 
             // execution
