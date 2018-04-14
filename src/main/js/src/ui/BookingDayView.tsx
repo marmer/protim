301325e47319ebe50dcfea6f94b621819eec13f7
MarMer <1055445+marmer@users.noreply.github.com>
2018-04-14 11:37:04 +0200

    Sketches of some layout options created for the bookind day view

--- src/main/js/src/ui/BookingDayView.tsx
+++ src/main/js/src/ui/BookingDayView.tsx
@@ -17,38 +17,68 @@ export class BookingDayView extends React.Component<BookingDayViewProps, Booking
 
     render(): React.ReactNode {
         return (
-            <div className="container">
+            <div>
                 <input
                     type="date"
                     name="bookingDay"
                     value={moment(this.props.day).format('YYYY-MM-DD')}
                     disabled={true}
                 />
-                <div className="row form-group align-items-center">
+                <h1>Option 1 - Grid Layout</h1>
+                <div className="container">
+                    <div className="row form-group align-items-center">
+                        {/*start time*/}
+                        <input
+                            type="time"
+                            className="col-1 form-control starttime"
+                            placeholder="1d 2h 25m"
+                        />
+                        {/*duration*/}
+                        <input
+                            type="time"
+                            className="col-1 form-control"
+                            placeholder="1d 2h 25m"
+                        />
+                        {/*description*/}
+                        <textarea className="col form-control" placeholder="What has been done"/>
+                        {/*notes*/}
+                        <textarea className="col form-control" placeholder="personal notes"/>
+                        {/*assignment ticket system*/}
+                        <select className="col-2 form-control">
+                            <option>None</option>
+                            <option>Jira</option>
+                        </select>
+                        {/*is booked button*/}
+                        <button type="col-1 button" className="btn">X</button>
+                    </div>
+                </div>
+                <h1>Option 2 - Option Input Group</h1>
+                <div className="input-group">
                     {/*start time*/}
                     <input
                         type="time"
-                        className="col-1 form-control starttime"
+                        className="input-group-prepend starttime"
                         placeholder="1d 2h 25m"
                     />
                     {/*duration*/}
                     <input
                         type="time"
-                        className="col-1 form-control"
-                        placeholder="1d 2h 25m"
+                        className=""
                     />
+
                     {/*description*/}
-                    <input type="text" className="col form-control" placeholder="What has been done"/>
+                    <textarea className="" placeholder="What has been done"/>
                     {/*notes*/}
-                    <input type="text" className="col form-control" placeholder="personal notes"/>
+                    <textarea className="" placeholder="personal notes"/>
                     {/*assignment ticket system*/}
-                    <select className="col-2 form-control">
+                    <select className="">
                         <option>None</option>
                         <option>Jira</option>
                     </select>
                     {/*is booked button*/}
-                    <button type="col-1 button" className="btn">X</button>
+                    <button type="button" className="btn input-group-append">X</button>
                 </div>
+
             </div>
         );
         // TODO this component BookingDayView needs some sense
