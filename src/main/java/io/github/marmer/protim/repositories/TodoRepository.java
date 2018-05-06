cb96d960f584529c2a4e4bbeef85019d67389cf3
MarMer <1055445+marmer@users.noreply.github.com>
2018-05-06 10:25:43 +0200

    some old stuff removed and a basic spring security condiguration added from tut

--- src/main/java/io/github/marmer/protim/repositories/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package io.github.marmer.protim.repositories;
-
-import io.github.marmer.protim.model.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<Todo, Integer> {
-}
--- src/main/resources/db/migration/V4__todo.sql
+++
@@ -1,5 +0,0 @@
-create table TODO (
-  id SERIAL PRIMARY KEY,
-  description varchar(100) NOT NULL,
-  created_on timestamp NOT NULL
-);
