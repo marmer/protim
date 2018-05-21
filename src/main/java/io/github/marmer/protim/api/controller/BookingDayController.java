6d3a9943ac630e0953f8d063e5212b54fb3c0729
MarMer <1055445+marmer@users.noreply.github.com>
2018-05-21 21:49:19 +0200

    a little refactoring

--- src/main/java/io/github/marmer/protim/api/controller/BookingDayController.java
+++ src/main/java/io/github/marmer/protim/api/controller/BookingDayController.java
@@ -6,16 +6,14 @@ import io.github.marmer.protim.service.crud.BookingDayService;
 import io.github.marmer.protim.service.model.BookingDay;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.Optional;
 
 @RestController
@@ -26,21 +24,9 @@ public class BookingDayController {
     private final BookingDayService bookingDayService;
     private final Converter<BookingDay, BookingDayDTO> toBookingDayDTOConverter;
 
-    @GetMapping("/{day}")
-    public ResponseEntity<BookingDayDTO> getDay(@PathVariable("day") final String day) {
-
-
-        final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        final Date date;
-        try {
-            date = sdf.parse(day);
-        } catch (final ParseException e) {
-            return ResponseEntity.notFound().build();
-        }
-        final Calendar cal = Calendar.getInstance();
-        cal.setTime(date);
-
-        final Optional<BookingDay> bookingDay = bookingDayService.getBookingDay(cal);
+    @GetMapping("/{day:\\d{4}-\\d{2}-\\d{2}}")
+    public ResponseEntity<BookingDayDTO> getDay(@PathVariable("day") @DateTimeFormat(pattern = "yyyy-MM-dd", iso = DateTimeFormat.ISO.DATE) final Calendar day) {
+        final Optional<BookingDay> bookingDay = bookingDayService.getBookingDay(day);
         return bookingDay
                 .map(bookingDay1 -> ResponseEntity.ok(toBookingDayDTOConverter.convert(bookingDay1)))
                 .orElseGet(() -> ResponseEntity.notFound().build());
