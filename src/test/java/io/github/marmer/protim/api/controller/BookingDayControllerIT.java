c2e460f522f954b08f05eb8493f8dfb2c793cc13
MarMer <1055445+marmer@users.noreply.github.com>
2018-06-10 20:02:01 +0200

    changed requirements

--- src/test/java/io/github/marmer/protim/api/controller/BookingDayControllerIT.java
+++ src/test/java/io/github/marmer/protim/api/controller/BookingDayControllerIT.java
@@ -18,13 +18,14 @@ import org.springframework.test.context.junit4.rules.SpringMethodRule;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.Month;
 
+import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -56,11 +57,9 @@ public class BookingDayControllerIT {
     public void testGetDay_MultipleDaysExist_ShouldShowRequestedDay()
             throws Exception {
         // Preparation
-        final BookingDBO booking = new BookingDBO().setDescription("bookingDescription");
         bookingDayRepository.save(
                 new BookingDayDBO()
-                        .setDay(LocalDate.of(1985, Month.JANUARY, 2))
-                        .setBookings(singletonList(booking)));
+                        .setDay(LocalDate.of(1985, Month.JANUARY, 2)));
         bookingDayRepository.save(
                 new BookingDayDBO()
                         .setDay(LocalDate.of(1985, Month.JANUARY, 3)));
@@ -76,11 +75,13 @@ public class BookingDayControllerIT {
     public void testGetEntries_BookingsForMultipleDaysExist_ShouldListBookingsForRequestedDays()
             throws Exception {
         // Preparation
-        final BookingDBO booking = new BookingDBO().setDescription("bookingDescription");
+        final BookingDBO booking1 = new BookingDBO().setStartTime(LocalTime.of(18, 45));
+        final BookingDBO booking2 = new BookingDBO().setStartTime(LocalTime.of(19, 30));
+        final BookingDBO booking3 = new BookingDBO().setStartTime(LocalTime.of(13, 15));
         bookingDayRepository.save(
                 new BookingDayDBO()
                         .setDay(LocalDate.of(1985, Month.JANUARY, 2))
-                        .setBookings(singletonList(booking)));
+                        .setBookings(asList(booking1, booking2, booking3)));
         bookingDayRepository.save(
                 new BookingDayDBO()
                         .setDay(LocalDate.of(1985, Month.JANUARY, 3))
@@ -91,7 +92,9 @@ public class BookingDayControllerIT {
         mockMvc.perform(get("/api/day/1985-01-02/bookings"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpect(jsonPath("$.ids", contains(booking.getId().intValue())));
+                .andExpect(jsonPath("$", contains(
+                        booking3.getStartTime(),
+                        booking1.getStartTime(),
+                        booking2.getStartTime())));
     }
 }
