6d19b47a3d2bb589fdd1e52a2836fec392988c60
MarMer <1055445+marmer@users.noreply.github.com>
2018-07-07 02:31:27 +0200

    static cors configuration added

--- src/main/java/io/github/marmer/protim/api/configuration/SecurityConfig.java
+++ src/main/java/io/github/marmer/protim/api/configuration/SecurityConfig.java
@@ -1,11 +1,17 @@
 package io.github.marmer.protim.api.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -24,6 +30,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic();
     }
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        // TODO: marmer 07.07.2018 Take origins from Configuration instead of a hard coded one
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("https://marmer.online", "https://marmer.github.io"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
     @Override
     protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
