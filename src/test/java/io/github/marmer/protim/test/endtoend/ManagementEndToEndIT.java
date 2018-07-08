78335bd559461c0249ea8c47eeb7e55b72a56001
MarMer <1055445+marmer@users.noreply.github.com>
2018-07-08 18:20:28 +0200

    end to end tests split

--- src/test/java/io/github/marmer/protim/test/endtoend/ManagementEndToEndIT.java
+++ src/test/java/io/github/marmer/protim/test/endtoend/ManagementEndToEndIT.java
@@ -1,5 +1,6 @@
 package io.github.marmer.protim.test.endtoend;
 
+import io.github.marmer.protim.api.configuration.Role;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -11,14 +12,15 @@ import org.springframework.test.context.junit4.rules.SpringClassRule;
 import org.springframework.test.context.junit4.rules.SpringMethodRule;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.options;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @WithMockUser(roles = "USER")
-public class SecurityEndToEndIT {
+public class ManagementEndToEndIT {
     @ClassRule
     public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
     @Rule
@@ -28,31 +30,32 @@ public class SecurityEndToEndIT {
     private MockMvc mockMvc;
 
     @Test
-    public void testGetBookings_CorsConfigurationMatchingToRequest_ShouldNotAllowAccess()
+    public void testAtManagementApi_CallWithUser_ShouldBeForbidden()
             throws Exception {
         // Execution
         mockMvc.perform(
-                options("/api/day")
-                        .header("Access-Control-Request-Method", "GET")
-                        .header("Origin", "http://www.unknownURL.com"))
+                get("/management")
+                        .with(user("someone").roles(Role.USER)))
                 .andExpect(status().isForbidden());
     }
 
     @Test
-    public void testGetBookings_CorsConfigurationNotMatchingToRequest_ShouldAllowAccess()
+    public void testAtManagementApi_CallWithAdmin_ShouldWork()
             throws Exception {
         // Execution
         mockMvc.perform(
-                options("/api/day")
-                        .header("Access-Control-Request-Method", "GET")
-                        .header("Origin", "https://marmer.online"))
+                get("/management")
+                        .with(user("someone").roles(Role.ADMIN)))
                 .andExpect(status().isOk());
     }
 
     @Test
-    public void testLogout_UrlHasBeenCalled_ShouldSendStatusToForceTheBrowserToClearCredentials()
+    public void testAtManagementApi_CallWithAnonymous_ShouldWork()
             throws Exception {
         // Execution
-        mockMvc.perform(get("/logout")).andExpect(status().isUnauthorized());
+        mockMvc.perform(
+                get("/management")
+                        .with(anonymous()))
+                .andExpect(status().isUnauthorized());
     }
 }
