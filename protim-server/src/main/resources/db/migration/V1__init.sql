1dcfd15c85a4250d6ebfde644480d39510935ae9
MarMer <1055445+marmer@users.noreply.github.com>
2018-08-11 01:02:05 +0200

    refactoring into simple multi module project

---
+++ protim-server/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,31 @@
+create table booking_days (
+  id      bigserial primary key,
+  day     date not null,
+  version bigint
+);
+
+create table bookings (
+  id          bigserial primary key,
+  fk_day         BIGINT references booking_days (id),
+  start_time  time,
+  duration    time,
+  description varchar,
+  notes       varchar,
+  ticket      varchar,
+  version     bigint
+);
+
+create table users (
+  id       bigserial primary key,
+  username varchar unique,
+  password varchar,
+  enabled  boolean,
+  version  bigint
+);
+
+create table roles (
+  id      bigserial primary key,
+  fk_user    bigint references users (id),
+  name    varchar,
+  version bigint
+);
