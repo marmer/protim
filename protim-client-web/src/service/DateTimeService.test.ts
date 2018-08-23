a8129d57e9f18f18b1e6358d19d19853c36a4681
MarMer <1055445+marmer@users.noreply.github.com>
2018-08-23 23:10:59 +0200

    some removed tests added again

---
+++ protim-client-web/src/service/DateTimeService.test.ts
@@ -0,0 +1,41 @@
+import {DateTimeService} from "./DateTimeService";
+
+describe('Tests for: DateTimeService', () => {
+    describe("day before is requested", () => {
+        describe("day with beginning of a month given", () => {
+            it("should return the last day of the month before", () => {
+                const givenDate = new Date(1985, 4, 1);
+
+                const result: Date = DateTimeService.dayBefore(givenDate);
+
+                expect(result).toEqual(new Date(1985, 3, 30));
+            });
+
+        });
+        it("the resulting object should not be the same instance as the given one", () => {
+            const givenDate = new Date(1985, 5, 1);
+            const result: Date = DateTimeService.dayBefore(givenDate);
+
+            expect(result).not.toBe(givenDate);
+        });
+    });
+
+    describe("day after is requested", () => {
+        describe("day with ending of a month given", () => {
+            it("should return the last day of the month before", () => {
+                const givenDate = new Date(1985, 2, 31);
+                const result: Date = DateTimeService.dayAfter(givenDate);
+
+                expect(result).toEqual(new Date(1985, 3, 1));
+            });
+
+        });
+        it("the resulting object should not be the same instance as the given one", () => {
+            const givenDate = new Date(1985, 4, 1);
+            const result: Date = DateTimeService.dayAfter(givenDate);
+
+            expect(result).not.toBe(givenDate);
+        });
+    });
+
+});
