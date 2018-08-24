622dc579c909b064d2f0ba156b3e7ea538627cf7
MarMer <1055445+marmer@users.noreply.github.com>
2018-08-24 17:48:42 +0200

    compile errors fixed

--- protim-client-web/src/model/usermanagement/User.ts
+++ protim-client-web/src/model/usermanagement/User.ts
@@ -1,7 +1,14 @@
 export class User {
-    username: string;
-    password: string;
-    enabled: boolean;
-    roles: string[]
+    username?: string;
+    password?: string;
+    enabled?: boolean;
+    roles?: string[];
+
+    constructor(username?: string, password?: string, enabled?: boolean, roles?: string[]) {
+        this.username = username;
+        this.password = password;
+        this.enabled = enabled;
+        this.roles = roles;
+    }
 
 }
