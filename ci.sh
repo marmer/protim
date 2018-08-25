027b0800cbd867b0566bc5a3fbebacd371ab44cf
MarMer <1055445+marmer@users.noreply.github.com>
2018-08-25 14:06:25 +0200

    no need for updates here

--- ci.sh
+++ ci.sh
@@ -5,15 +5,15 @@ set -e
 echo
 echo --- Building ---
 echo
-mvn clean install -DskipTests -Dmaven.javadoc.skip -Dmaven.source.skip -Dassembly.skipAssembly -B -V -U -T 2.0C
+mvn clean install -DskipTests -Dmaven.javadoc.skip -Dmaven.source.skip -Dassembly.skipAssembly -B -V -U -T 1.5C
 
 echo
 echo --- Testing ---
 echo
-mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify -V -U -T 2.0C
+mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify -V -T 1.5C
 
 echo
 echo --- Reporting ---
 echo
 cp /tmp/nodebin/node/node /bin/
-mvn org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -Dmaven.javadoc.skip -Dmaven.source.skip -Dassembly.skipAssembly -V -Dsonar.login=$SONAR_CLOUD_TOKEN -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=marmer-github -V -U -T 2.0C
+mvn org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -Dmaven.javadoc.skip -Dmaven.source.skip -Dassembly.skipAssembly -V -Dsonar.login=$SONAR_CLOUD_TOKEN -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=marmer-github -V -T 1.5C
