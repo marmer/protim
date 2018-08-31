c3b925aac44e789f87f8a341fcb0f86479a93b9e
MarMer <1055445+marmer@users.noreply.github.com>
2018-08-31 18:56:57 +0200

    a little navbar preparation with some comments what todo

--- protim-client-web/src/Router.tsx
+++ protim-client-web/src/Router.tsx
@@ -9,7 +9,24 @@ class Router extends React.Component {
         return (
             <div className="App">
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                    {/*todo User React Link for navigation*/}
                     <a className="navbar-brand" href="#">Protim</a>
+                    <div className="collapse navbar-collapse">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                {/*todo User React Link for navigation*/}
+                                {/*Make active when in /timetracking*/}
+                                {/*Show only when the current user is an admin*/}
+                                <a className="nav-link" href="#">Pricing</a>
+                            </li>
+                            <li className="nav-item">
+                                {/*todo User React Link for navigation*/}
+                                {/*Make active when in /user*/}
+                                {/*Show only when the current user is a user*/}
+                                <a className="nav-link" href="#">Usermanagement</a>
+                            </li>
+                        </ul>
+                    </div>
                 </nav>
                 <BrowserRouter>
                     <Switch>
