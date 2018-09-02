f697ee97a1f75ad4dd3828515e483bdde8c2db2b
MarMer <1055445+marmer@users.noreply.github.com>
2018-09-02 21:19:36 +0200

    navbar moved to an own file

---
+++ protim-client-web/src/HeaderView.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+
+export interface HeaderViewProps {
+}
+
+export interface HeaderViewState {
+}
+
+export class HeaderView extends React.Component<HeaderViewProps, HeaderViewState> {
+    constructor(props: HeaderViewProps) {
+        super(props);
+    }
+
+    render(): React.ReactNode {
+        // TODO this component HeaderView needs some sense
+        return (
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                {/*todo User React Link for navigation*/}
+                <a className="navbar-brand" href="#">Protim</a>
+                {/*todo Make Toggler somehow*/}
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarText"
+                    aria-controls="navbarText"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"/>
+                </button>
+                <div className="collapse navbar-collapse">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            {/*todo User React Link for navigation*/}
+                            {/*Make active when in /timetracking*/}
+                            {/*Show only when the current user is an admin*/}
+                            <a className="nav-link" href="#">Pricing</a>
+                        </li>
+                        <li className="nav-item">
+                            {/*todo User React Link for navigation*/}
+                            {/*Make active when in /user*/}
+                            {/*Show only when the current user is a user*/}
+                            <a className="nav-link" href="#">Usermanagement</a>
+                        </li>
+                    </ul>
+                    <span className="navbar-text">
+      Navbar text with an inline element
+                        </span>
+                </div>
+            </nav>
+        );
+    }
+}
--- protim-client-web/src/Router.tsx
+++ protim-client-web/src/Router.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {Route, Switch} from 'react-router';
 import {BrowserRouter} from "react-router-dom";
+import {HeaderView} from "./HeaderView";
 import {TimetrackingView} from './ui/TimetrackingView';
 import {UsermanagementView} from "./ui/UsermanagementView";
 
@@ -8,41 +9,7 @@ class Router extends React.Component {
     render() {
         return (
             <div className="App">
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                    {/*todo User React Link for navigation*/}
-                    <a className="navbar-brand" href="#">Protim</a>
-                    {/*todo Make Toggler somehow*/}
-                    <button
-                        className="navbar-toggler"
-                        type="button"
-                        data-toggle="collapse"
-                        data-target="#navbarText"
-                        aria-controls="navbarText"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <span className="navbar-toggler-icon"/>
-                    </button>
-                    <div className="collapse navbar-collapse">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                {/*todo User React Link for navigation*/}
-                                {/*Make active when in /timetracking*/}
-                                {/*Show only when the current user is an admin*/}
-                                <a className="nav-link" href="#">Pricing</a>
-                            </li>
-                            <li className="nav-item">
-                                {/*todo User React Link for navigation*/}
-                                {/*Make active when in /user*/}
-                                {/*Show only when the current user is a user*/}
-                                <a className="nav-link" href="#">Usermanagement</a>
-                            </li>
-                        </ul>
-                        <span className="navbar-text">
-      Navbar text with an inline element
-                        </span>
-                    </div>
-                </nav>
+                <HeaderView/>
                 <BrowserRouter>
                     <Switch>
                         <Route path="/user(/:user)?" component={UsermanagementView}/>
