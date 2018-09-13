2cc633fbfbece67b7633b96f7bdb5731d508b83e
MarMer <1055445+marmer@users.noreply.github.com>
2018-09-13 11:29:54 +0200

    some preparation

--- protim-server/src/test/java/io/github/marmer/protim/api/selfinformation/MeControllerTest.java
+++ protim-server/src/test/java/io/github/marmer/protim/api/selfinformation/MeControllerTest.java
@@ -1,5 +1,13 @@
-import static org.junit.Assert.*;
+package io.github.marmer.protim.api.selfinformation;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest
 public class MeControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
 
+    
 }
--- protim-server/src/test/java/io/github/marmer/protim/persistence/relational/usermanagement/RoleDBOTestdata.java
+++ protim-server/src/test/java/io/github/marmer/protim/persistence/relational/usermanagement/RoleDBOTestdata.java
@@ -1,10 +1,13 @@
 package io.github.marmer.protim.persistence.relational.usermanagement;
 
 import io.github.benas.randombeans.EnhancedRandomBuilder;
+import io.github.benas.randombeans.FieldDefinition;
 import io.github.benas.randombeans.api.EnhancedRandom;
 
 public class RoleDBOTestdata {
     private final static EnhancedRandom builder = EnhancedRandomBuilder.aNewEnhancedRandomBuilder()
+            .exclude(new FieldDefinition<>("id", Long.class, RoleDBO.class))
+            .exclude(new FieldDefinition<>("version", Long.class, RoleDBO.class))
             .seed(42)
             .build();
 
